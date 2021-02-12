8b67ff48807db9c76781d54626839e737143cb3c
smsaifulalom <35555808+smsaifulalom@users.noreply.github.com>
2021-02-12 20:17:49 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// DOM Manipulation
+
+
+const progress = document.querySelector('.progress-load');
+
+
+    progress.style.opacity = 1
+    progress.style.width = progress.getAttribute("data-done") + "%"
+
+
